2cfd2e228c382a2a72c23ab398e4c9632925e363
Karen Shiloh Jolly <karenshilohj@gmail.com>
2022-12-13 18:26:52 -0800

    changes

--- fetch.js
+++ fetch.js
@@ -1,6 +1,6 @@
 // create api-key.js file with const API_KEY="your_api_key" in this same directory to use
 const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-const API_KEY = 'CzgFs866JsEt87jzvQ5K7IgbR7TrLAqR';
+const API_KEY = 'uAMpz9JuorjDSsXftAGf7JNoltIGj3ca';
 const url = `${BASE_URL}?q=tech&api-key=${API_KEY}`;
 
 fetch(url)
