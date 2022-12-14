b31c4886d4821c75a074c5417d83450be9050de4
Karen Shiloh Jolly <karenshilohj@gmail.com>
2022-12-13 18:42:44 -0800

    more working...

--- best-books.js
+++ best-books.js
@@ -1,19 +1,45 @@
 // create api-key.js file with const API_KEY="your_api_key" in this same directory to use
-const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+const BASE_URL = 'https://api.nytimes.com/svc/books/v3/lists/'
+//const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+const API_KEY = 'CzgFs866JsEt87jzvQ5K7IgbR7TrLAqR';
 
-const url = `${BASE_URL}?q=tech&api-key=${API_KEY}`;
 
 const formEl = document.getElementById('best-books-form');
 const yearEl = document.getElementById('year');
 const monthEl = document.getElementById('month');
 const dateEl = document.getElementById('date');
 
-formEl.addEventListener('submit', function(e) {
+formEl.addEventListener('submit', function (e) {
   e.preventDefault();
 
   const year = yearEl.value;
   const month = monthEl.value;
   const date = dateEl.value;
 
+  console.log('the year is ', year);
+
   // Fetch bestselling books for date and add top 5 to page
+  const url = `${BASE_URL}${year}-${month}-${date}/hardcover-fiction.json?api-key=${API_KEY}`;
+  console.log('the url is as follows');
+  console.log(url);
+  fetch(url)
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (responseJson) {
+      console.log(responseJson);
+
+      let book = responseJson.results.books[0].title;
+      console.log(book);
+
+      //const mainHeadline = article.headline.main;
+      //document.getElementById('article-title').innerText = mainHeadline;
+
+     // if (article.multimedia.length > 0) {
+      //  const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
+       // document.getElementById('article-img').src = imgUrl;
+     // }
+    });
 });
+
+
